0cb1d8b416a97a0522ac3d7a1e7cf700cd86cb29
David <david.huet.j.e@gmail.com>
2016-11-15 17:56:18 +0100

    first

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Dehumanized</title>
+		<link rel="stylesheet" type="text/css" href="reset.css">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+
+	<body>
+
+		<h1>Dehumanized</h1>
+		
+		<ul>
+			<li>Aceuil</li>
+			<li>Presentation de l'équipe</li>
+			<li>Prensentation du film</li>
+		</ul>
+
+		<h2>Acueil</h2>
+		<p>Bonjour et bienvenue sur le site de Dehumanized, un court metrage écrit et réalisé par les MMI2C1 de l'Iut de Lens.</p>
+		<h3>Synopsie:</h3>
+		<p>L'histoire prend place dans un monde.</p>
+	</body>
+</html>
