03e7c0791ca23b145d295c9368a1a0145838e2ef
David <david.huet.j.e@gmail.com>
2016-11-20 23:38:56 +0100

    a completer

--- index.php
+++ index.php
@@ -13,13 +13,18 @@
 		
 		<ul>
 			<li>Aceuil</li>
-			<li>Presentation de l'équipe</li>
-			<li>Prensentation du film</li>
+			<li>Synopsie</li>
+			<li>Prensentation de l'équipe</li>
 		</ul>
 
 		<h2>Acueil</h2>
 		<p>Bonjour et bienvenue sur le site de Dehumanized, un court metrage écrit et réalisé par les MMI2C1 de l'Iut de Lens.</p>
-		<h3>Synopsie:</h3>
-		<p>L'histoire prend place dans un monde.</p>
+		<h2>Synopsie:</h2>
+		<p>L'histoire prend place dans un monde où la population a perdu tout sentiments et péchées. Eddy, un geek qui est resté 3 ans dans sa chambre pour terminer skyrim, décide de sortir puis découvre le monde actuel.</p>
+		<h2>Prensentation de l'équipe</h2>
+		<ul>
+			<li>Réalisateur: Antoine et Aurélien</li>
+			<li>...</li>
+		</ul>
 	</body>
 </html>
