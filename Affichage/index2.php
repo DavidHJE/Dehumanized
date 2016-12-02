ede623574ef5c739d35c28478d1a5b9b8ce0487b
mmmaxou <mmmaxou@yahoo.fr>
2016-12-02 01:52:52 +0100

    ajout de lindex2

---
+++ Affichage/index2.php
@@ -0,0 +1,14 @@
+<?php require "header.php"; ?>
+<?php require "menu.php"; ?>
+
+<div class="test">
+    <p class="first">Individu</p>
+    <p class="second">Vous êtes une erreur protocolaire</p>
+    <p class="third">Je me dois de vous arrêter</p>
+    <p class="underscore"></p>
+</div>
+
+<div class="menu"></div>
+
+<script type="text/javascript" src="../script/jquery-2.2.4.min.js"></script>
+<script type="text/javascript" src="../script/main.js"></script>
