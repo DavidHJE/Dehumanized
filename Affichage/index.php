397b252669474db08277df3097847deb9fea7144
David <david.huet.j.e@gmail.com>
2016-12-03 18:24:57 +0100

    Julien

--- Affichage/index.php
+++ Affichage/index.php
@@ -1,7 +1,7 @@
 <?php require "header.php"; ?>
 <?php require "menu.php"; ?>
 		
-<div id="Synopsis">
+<!--<div id="Synopsis">
 	<h2>Synopsis:</h2>
 	<p>L'histoire prend place dans un monde où la population a perdu tout sentiments et péchées. Eddy, un geek qui est resté 3 ans dans sa chambre pour terminer skyrim, décide de sortir puis découvre le monde actuel.</p>
 </div>
@@ -10,6 +10,23 @@
 	<h2>Trailer:</h2>
 </div>
 
-<div class="test"></div>
+<div class="test"></div> -->
+
+
+<section class="container homepage">
+	<div class="row">
+		<a href="#" class="col-md-6 synopsis" >
+			<h2>Synopsis:</h2>
+			<p>Une société entièrement normée. Plus d'émotions, d'originalité... C'est là que se retrouve Eddy, jeune
+ 			homme ayant passé ces dernières années enfermées dans sa chambre pour finir intégralement Skyrim. 
+			Devenu un paria, il doit fuir avec un mystérieux scientifique...
+			
+			</p>
+		<a href="#" class="col-md-6 trailer" >
+			<video>
+				<source src="http://demo.prognplay.com/mmi/html-s3/video-cosmo.mp4" type="video/mp4">
+				<source src="http://demo.prognplay.com/mmi/html-s3/video-cosmo.webm" type="video/webm">
+      			<source src="http://demo.prognplay.com/mmi/html-s3/video-cosmo.ogv" type="video/ogg">
+			</video>
 		
 <?php require "footer.php"; ?>
