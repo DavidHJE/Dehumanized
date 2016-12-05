553cd012f76af07000b4d28f83984bf0c98a9a96
David <david.huet.j.e@gmail.com>
2016-12-05 21:51:25 +0100

    Viseur

--- Affichage/menu.php
+++ Affichage/menu.php
@@ -1,11 +1,11 @@
 <div id="menu">
     <ul>
-        <a href="#" ><li class="glitch" data-text="Accueil">Accueil</li></a>
-        <a href="#" ><li class="glitch" data-text="Jeu">Jeu</li></a>
-        <a href="#" ><li class="glitch" data-text="Produit dérivé">Produit dérivé</li></a>
-        <a href="#" ><li class="glitch" data-text="L'équipe">L’équipe</li></a>
-        <a href="#" ><li class="glitch" data-text="Dossier de presse">Dossier de presse</li></a>
-        <a href="#" ><li class="glitch" data-text="Vidéo">Vidéo</li></a>
+        <a href="index.php" ><li class="glitch" data-text="Accueil">Accueil</li></a>
+        <a href="jeu.php" ><li class="glitch" data-text="Jeu">Jeu</li></a>
+        <a href="Shop.php" ><li class="glitch" data-text="Produit dérivé">Produit dérivé</li></a>
+        <a href="equipe.php" ><li class="glitch" data-text="L'équipe">L’équipe</li></a>
+        <a href="dossierDePresse.php" ><li class="glitch" data-text="Dossier de presse">Dossier de presse</li></a>
+        <a href="video.php" ><li class="glitch" data-text="Vidéo">Vidéo</li></a>
     </ul>
 
     <button id="go">&raquo; Run</button>
