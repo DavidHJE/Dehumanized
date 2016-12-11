bd5b41fc2aee44441bc96216889f9fce74633e75
David <david.huet.j.e@gmail.com>
2016-12-11 23:16:34 +0100

    Je recommence

--- Affichage/Shop.php
+++
@@ -1,7 +0,0 @@
-<?php require "header.php"; ?>
-<?php require "menu.php"; ?>
-<ul>
-	<li><img src="../Image/sheep.jpg"></li>
-	<li><img src="../Image/La-folie-des-produits-dérivés-Pantone-10.jpg"></li>
-</ul>
-<?php require "footer.php"; ?>
--- Affichage/video.php
+++
@@ -1,7 +0,0 @@
-<?php require "header.php"; ?>
-<?php require "menu.php"; ?>
-
-<iframe width="400" height="250" src="https://www.youtube.com/embed/qV5lzRHrGeg" frameborder="0" allowfullscreen></iframe>
-<iframe width="400" height="250" src="https://www.youtube.com/embed/qV5lzRHrGeg" frameborder="0" allowfullscreen></iframe>
-<iframe width="400" height="250" src="https://www.youtube.com/embed/qV5lzRHrGeg" frameborder="0" allowfullscreen></iframe>
-<?php require "footer.php"; ?>
